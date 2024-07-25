a335a84eb3429822cfe0420a3b1647fd66bfe16b
ccyuen1 <143187971+ccyuen1@users.noreply.github.com>
2024-07-26 00:50:01 +0800

    CLI draft

---
+++ src/cli.rs
@@ -0,0 +1,42 @@
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, author)]
+/// A simple program to create, view, and modify an encrypted file in a specific format
+pub struct Cli {
+    #[command(subcommand)]
+    pub action: Actions,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Actions {
+    /// Create a encrypted file
+    Create(CreateArgs),
+
+    /// Open an encrypted file
+    Open(OpenArgs),
+
+    /// Alias of create
+    C(CreateArgs),
+
+    /// Alias of open
+    O(OpenArgs),
+}
+
+#[derive(Args, Debug)]
+pub struct CreateArgs {
+    /// Path to the encrypted file to create
+    pub file: PathBuf,
+}
+
+#[derive(Args, Debug)]
+pub struct OpenArgs {
+    /// Path to the encrypted file to open
+    pub file: PathBuf,
+
+    #[arg(short, long)]
+    /// Program to open the decrypted file
+    pub executable: Option<PathBuf>,
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use clap::Parser;
+use cli::Cli;
+
+mod cli;
+
+fn main() {
+    let cli = Cli::parse();
+
+    println!("{:?}", cli);
+}
