c28d23eff6fd3f8081749921610e5e3d9cb4f447
ccyuen1 <143187971+ccyuen1@users.noreply.github.com>
2024-07-27 16:34:10 +0800

    Work on the create functionality

--- src/create.rs
+++ src/create.rs
@@ -1,3 +1,7 @@
+use std::{fs::OpenOptions, io::Write};
+
+use zeroize::Zeroize;
+
 use crate::cli::CreateArgs;
 
 /// Create a new encrypted file in the given path.
@@ -5,9 +9,38 @@ use crate::cli::CreateArgs;
 ///
 /// # Examples
 /// ```no_run
-/// encrypted_file_manager::create(CreateArgs { file: "example.encrypted".into() })
+/// create(CreateArgs { file: "example.encrypted".into() })?;
 /// ```
 pub fn create(args: &CreateArgs) -> Result<(), Box<dyn std::error::Error>> {
-    let password = rpassword::prompt_password("Password: ")?;
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&args.file)?;
+    write_header(&mut file)?;
+
+    let mut password =
+        rpassword::prompt_password("Create a password for the file: ")?;
+
+    password.zeroize();
+
+    todo!()
+}
+
+/// Write the header of the encrypted file.
+fn write_header(writer: &mut impl Write) -> std::io::Result<()> {
+    writer.write_all(b"0.1\nencrypted-file-format\n\n")?;
+    writer.flush()?;
+    Ok(())
+}
+
+/// Write the metadata section of the encrypted file.
+fn write_metadata(
+    writer: &mut impl Write,
+    compression_enabled: bool,
+    salt: &[u8; 32],
+    nonce_dek: &[u8; 12],
+    nonce_body: &[u8; 12],
+    encrypted_dek: &[u8; 48],
+) -> std::io::Result<()> {
     todo!()
 }
