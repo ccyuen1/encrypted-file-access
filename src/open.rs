2e0d66e6646fd27036148ae6d969d7ec9dff197a
ccyuen1 <143187971+ccyuen1@users.noreply.github.com>
2024-08-01 19:35:18 +0800

    draft the code structure for open

--- src/open.rs
+++ src/open.rs
@@ -1,7 +1,20 @@
-use std::path::PathBuf;
+use std::{
+    fs::File,
+    io,
+    ops::{Add, Sub},
+    path::PathBuf,
+};
 
+use aead::{
+    stream::{StreamBE32, StreamPrimitive},
+    AeadInPlace, KeySizeUser,
+};
+use aes_gcm_siv::Aes256GcmSiv;
 use clap::Args;
+use generic_array::ArrayLength;
 use open::commands;
+
+use crate::encrypted_file_format::{Header, Metadata};
 
 #[derive(Args, Debug)]
 /// Arguments for opening an existing encrypted file
@@ -14,6 +27,61 @@ pub struct OpenArgs {
     pub executable: Option<PathBuf>,
 }
 
+/// Decrypt a password-protected file to a temporary file
+/// and then open it with the specified application.
+///
+/// Modifications to the temporary file are reflected in the password-protected file
+/// after the application terminates.
+///
+/// # Examples
+/// ```no_run
+/// use std::path::PathBuf;
+/// use encrypted_file_access::open::{OpenArgs, open};
+/// let args = OpenArgs {
+///     file: PathBuf::from("example.encrypted"),
+///     executable: Some(PathBuf::from("notepad.exe")),
+/// };
+/// open(&args).unwrap();
+/// ```
 pub fn open(args: &OpenArgs) -> anyhow::Result<()> {
+    let mut in_file = File::open(&args.file)?;
+    let header = read_header(&mut in_file)?;
+    let metadata: Metadata<Aes256GcmSiv, StreamBE32<_>> =
+        read_metadata(&mut in_file)?;
+
+    // TODO: prepare a temporary file
+
+    // TODO: decrypt the body
+
+    // TODO: open the decrypted file with the specified application
+
+    // TODO: after the application terminates, write modifications back to the file
+
+    // TODO: clean up the temporary file
+
+    todo!()
+}
+
+/// Read and parse the header of an encrypted file.
+///
+/// This will error if the header is invalid.
+fn read_header(reader: &mut impl io::Read) -> anyhow::Result<Header> {
+    todo!()
+}
+
+/// Read and parse the metadata section of an encrypted file.
+///
+/// This will error if the metadata section is invalid.
+fn read_metadata<A, S>(
+    reader: &mut impl io::Read,
+) -> anyhow::Result<Metadata<A, S>>
+where
+    A: AeadInPlace + KeySizeUser,
+    S: StreamPrimitive<A>,
+    A::NonceSize: Sub<S::NonceOverhead>,
+    aead::stream::NonceSize<A, S>: ArrayLength<u8>,
+    A::KeySize: Add<A::TagSize>,
+    <A::KeySize as Add<A::TagSize>>::Output: ArrayLength<u8>,
+{
     todo!()
 }
